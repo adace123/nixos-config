cde1012afbb25c20093dfbae51bbc8296ebee67e
aaron.feigenbaum <aaron@enervee.com>
2023-04-14 01:15:10 -0700

    remove comment

--- hosts/hp-pavilion/hardware-configuration.nix
+++ hosts/hp-pavilion/hardware-configuration.nix
@@ -5,7 +5,6 @@
   modulesPath,
   ...
 }: {
-  # TODO: Change device to correct name
   modules.hardware.device = "/dev/sda";
 
   boot.initrd.availableKernelModules = ["nvme" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod"];
