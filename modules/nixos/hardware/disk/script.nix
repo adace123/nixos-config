5b505655cbbd3833924a16dad7b62284f1ac0264
aaron.feigenbaum <aaron@enervee.com>
2023-04-14 22:30:52 -0700

    try out new script

--- modules/nixos/hardware/disk/script.nix
+++ modules/nixos/hardware/disk/script.nix
@@ -3,31 +3,30 @@
   lib,
   pkgs,
   ...
-}: let
-  disko = pkgs.writeShellScriptBin "disko" ''${config.system.build.disko}'';
-  disko-mount = pkgs.writeShellScriptBin "disko-mount" "${config.system.build.mountScript}";
-  disko-format = pkgs.writeShellScriptBin "disko-format" "${config.system.build.formatScript}";
-  install-system = pkgs.writeShellScriptBin "install-system" ''
-    set -euo pipefail
-
-    echo "Formatting disks..."
-    ${disko-format}/bin/disko-format
-
-    echo "Mounting disks..."
-    ${disko-mount}/bin/disko-mount
-
-    echo "Installing system..."
-    nixos-install --system x86_64-linux
-
-    echo "Done!"
-  '';
-in {
-  disko.enableConfig = lib.mkDefault false;
-  environment.systemPackages = [
-    disko
-    disko-mount
-    disko-format
-    install-system
-  ];
-
+}: {
+  system.build.install = pkgs.writeShellApplication {
+    name = "install";
+    text = ''
+      # copy disko script
+      nix --extra-experimental-features nix-command copy \
+        --from "https://cache.nichi.co" \
+        --option trusted-public-keys "hydra.nichi.co-0:P3nkYHhmcLR3eNJgOAnHDjmQLkfqheGyhZ6GLrUVHwk=" \
+        ${config.system.build.disko}
+      # run disko script
+      ${config.system.build.disko}
+      # copy sops key
+      mkdir -p /mnt/persist/var/lib/
+      (umask 0077 && curl -s http://169.254.169.254/latest/user-data -o /mnt/persist/var/lib/sops.key)
+      # install
+      nixos-install --root /mnt --system ${config.system.build.toplevel} \
+        --no-channel-copy --no-root-passwd \
+        --option extra-substituters "https://cache.nichi.co" \
+        --option trusted-public-keys "hydra.nichi.co-0:P3nkYHhmcLR3eNJgOAnHDjmQLkfqheGyhZ6GLrUVHwk="
+      reboot
+    '';
+    checkPhase = ''
+      mkdir -p $out/nix-support
+      echo "file install $out/bin/install" >> $out/nix-support/hydra-build-products
+    '';
+  };
 }
