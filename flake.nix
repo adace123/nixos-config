3af88a6147654ba6423b0cd1128cc1891b3964e0
aaron.feigenbaum <aaron@enervee.com>
2023-04-25 01:11:52 -0700

    use flake-parts

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nuenv.url = "github:DeterminateSystems/nuenv";
-    agenix.url = "github:ryantm/agenix";
+    flake-parts.url = "github:hercules-ci/flake-parts";
     sops-nix.url = "github:Mic92/sops-nix";
   };
 
@@ -23,35 +23,32 @@
     disko,
     nuenv,
     sops-nix,
+    flake-parts,
     ...
   } @ inputs: let
     system = "x86_64-linux";
     overlays = [nuenv.overlays.nuenv];
-    pkgs = import nixpkgs {inherit system overlays;};
-  in {
-    devShells.${system}.default = pkgs.mkShell {
-      packages = with pkgs; [just statix alejandra];
-    };
-
-    nixosConfigurations = {
-      coruscant = nixpkgs.lib.nixosSystem {
-        inherit system;
-        modules = [
-          ./hosts/coruscant
-          ./modules/nixos
-          sops-nix.nixosModules.sops
-        ];
-        specialArgs = {inherit inputs pkgs;};
+  in
+    flake-parts.lib.mkFlake {inherit inputs;} ({...}: {
+      systems = ["x86_64-linux" "x86_64-darwin"];
+      perSystem = {pkgs, ...}: {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [just statix alejandra sops];
+        };
       };
 
-      # vm = nixpkgs.lib.nixosSystem {
-      #   inherit system;
-      #   modules = [
-      #     ./hosts/vm
-      #     ./modules/nixos
-      #   ];
-      #   specialArgs = {inherit inputs pkgs;};
-      # };
-    };
-  };
+      flake = {
+        nixosConfigurations = {
+          coruscant = nixpkgs.lib.nixosSystem {
+            inherit system;
+            modules = [
+              ./hosts/coruscant
+              ./modules/nixos
+              sops-nix.nixosModules.sops
+            ];
+            specialArgs = {inherit inputs;};
+          };
+        };
+      };
+    });
 }
