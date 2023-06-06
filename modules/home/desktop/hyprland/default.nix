7e765e109657e0d6970b0edfdd5486a8d1c5f65b
aaron.feigenbaum <aaron@enervee.com>
2023-06-06 00:30:58 -0700

    remove wayland home session vars

--- modules/home/desktop/hyprland/default.nix
+++ modules/home/desktop/hyprland/default.nix
@@ -24,18 +24,6 @@ in
       };
 
       home = {
-        sessionVariables = {
-          WLR_NO_HARDWARE_CURSORS = "1";
-          XWAYLAND_NO_GLAMOR = "1";
-          _JAVA_AWT_WM_NONREPARENTING = "1";
-          MOZ_ENABLE_WAYLAND = "1";
-          QT_QPA_PLATFORM = "wayland:xcb";
-          QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
-          SDL_VIDEODRIVER = "wayland";
-          XDG_SESSION_TYPE = "wayland";
-          NIXOS_OZONE_WL = "1";
-        };
-
         packages = with pkgs; [
           # screenshot
           grim
