a795267fc22a7bf7f983b53218fc5d13e7533533
aaron.feigenbaum <aaron@enervee.com>
2023-06-19 16:46:21 -0700

    (flake): remove flake-parts, import custom pkgs and overlays

--- flake.nix
+++ flake.nix
@@ -10,10 +10,14 @@
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     nuenv.url = "github:DeterminateSystems/nuenv";
-    flake-parts.url = "github:hercules-ci/flake-parts";
     sops-nix.url = "github:Mic92/sops-nix";
     hyprland.url = "github:hyprwm/Hyprland";
     nix-colors.url = "github:misterio77/nix-colors";
+    nur.url = "github:nix-community/NUR";
+    amadeus-dotfiles-hyprland = {
+      url = "github:AmadeusWM/dotfiles-hyprland?rev=6818be21d8b1d193168bda85017c277393e00589";
+      flake = false;
+    };
   };
 
   outputs = {
@@ -21,22 +25,21 @@
     nixpkgs,
     disko,
     nuenv,
-    flake-parts,
     ...
   } @ inputs: let
-    defaultSystem = "x86_64-linux";
-  in
-    flake-parts.lib.mkFlake {inherit inputs;} (_: {
-      systems = ["x86_64-linux" "x86_64-darwin"];
-      perSystem = {pkgs, ...}: {
-        devShells.default = pkgs.mkShell {
-          packages = with pkgs; [just statix alejandra sops ssh-to-age pulumi];
-        };
-        # packages.infra = import ./infra {inherit pkgs;};
-      };
+    inherit (self) outputs;
+    overlays = with inputs; [
+      nuenv.overlays.nuenv
+      nur.overlay
+      outputs.overlays.default
+    ];
 
-      flake = {
-        nixosConfigurations = import ./hosts {inherit inputs nixpkgs;};
-      };
-    });
+    forEachSystem = nixpkgs.lib.genAttrs ["x86_64-linux" "x86_64-darwin"];
+    forEachPkgs = f: forEachSystem (system: f (import nixpkgs {inherit system overlays;}));
+  in {
+    packages = forEachPkgs (pkgs: (import ./pkgs {inherit pkgs inputs;}));
+    overlays = import ./overlays {inherit inputs;};
+    devShells = forEachPkgs (pkgs: import ./shell.nix {inherit pkgs;});
+    nixosConfigurations = import ./hosts {inherit inputs outputs nixpkgs;};
+  };
 }
