5f6b30e04f1b886b29609577f277da574a427897
aaron.feigenbaum <aaron@enervee.com>
2023-07-10 19:03:25 -0700

    feat(wayland): try to fix greetd issue

--- modules/nixos/windowmanager/default.nix
+++ modules/nixos/windowmanager/default.nix
@@ -5,32 +5,26 @@
   ...
 }: let
   cfg = config.modules.window-manager.hyprland;
-  inherit (config.modules.sys.graphics) nvidia;
 in
   with lib; {
     imports = [./greetd.nix];
     options.modules.window-manager.hyprland.enable = mkEnableOption "Enable Hyprland";
 
     config = mkIf cfg.enable {
-      security = {
-        pam.services.gtklock.text = ''
-          auth include login
-          auth sufficient pam_unix.so try_first_pass likeauth nullok
-        '';
-      };
-
       environment.sessionVariables = {
         WLR_NO_HARDWARE_CURSORS = "1";
         NIXOS_OZONE_WL = "1";
         XDG_SESSION_TYPE = "wayland";
-        QT_QPA_PLATFORM = "wayland";
+        QT_QPA_PLATFORM = "xcb";
+        LIBSEAT_BACKEND = "logind";
       };
 
       programs.hyprland.enable = true;
 
       xdg.portal = {
         enable = true;
-        extraPortals = [pkgs.xdg-desktop-portal-hyprland];
+        wlr.enable = true;
+        extraPortals = [pkgs.xdg-desktop-portal-gtk pkgs.xdg-desktop-portal-wlr pkgs.xdg-desktop-portal-hyprland];
       };
 
       security.polkit.enable = true;
