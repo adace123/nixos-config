0e3b723d9156c39b9b60cb3d4fcf5e09fd12c86e
adace123 <18275490+adace123@users.noreply.github.com>
2023-08-12 21:09:14 -0700

    feat: add default home packages

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -1,8 +1,13 @@
-{...}: {
+{pkgs, ...}: {
   imports = [
     ./desktop
     ./shell
     ./dev
     ./programs
+  ];
+
+  # default home packages
+  home.packages = with pkgs; [
+    mdcat
   ];
 }
