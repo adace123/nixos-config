83b22c4205ff37f0bc0728952d14f5538fc392fa
adace123 <18275490+adace123@users.noreply.github.com>
2023-08-17 22:42:31 -0700

    fix(git): try to fix signing

--- modules/home/dev/git.nix
+++ modules/home/dev/git.nix
@@ -43,8 +43,10 @@ in
             then "nvim"
             else "vim";
           push.autoSetupRemote = true;
-          user.signingkey = cfg.signingKey;
+          # user.signingkey = cfg.signingKey;
+          user.signingkey = "${config.home.homeDirectory}/.ssh/github_signing_key.pub";
           gpg.format = "ssh";
+          gpg.ssh.defaultKeyCommand = "ssh-add -L";
           commit.gpgsign = true;
         };
       };
