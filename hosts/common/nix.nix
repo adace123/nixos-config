cdcb9fc7302ebdcb5998297f62465f3f60d61233
adace123 <18275490+adace123@users.noreply.github.com>
2023-08-26 11:21:43 -0700

    feat(nix): add more substituters

--- hosts/common/nix.nix
+++ hosts/common/nix.nix
@@ -8,18 +8,32 @@ _: {
 
     settings = {
       auto-optimise-store = true;
+      builders-use-substitutes = true;
       experimental-features = ["nix-command" "flakes"];
       trusted-users = ["root" "@wheel"];
       substituters = [
         "https://cache.nixos.org"
+        "https://cache.garnix.io"
         "https://nix-community.cachix.org"
+        "https://hyprland.cachix.org"
       ];
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       ];
     };
   };
 
-  nixpkgs.config.allowUnfree = true;
+  nixpkgs = {
+    config.allowUnfree = true;
+    overlays = [
+      (
+        _: prev: {
+          zig = prev.zig.overrideAttrs (old: {version = "0.11.0";});
+        }
+      )
+    ];
+  };
 }
