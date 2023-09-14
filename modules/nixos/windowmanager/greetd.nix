54d4c0d99f23097346980563f8b4d1f0dbdd5616
adace123 <18275490+adace123@users.noreply.github.com>
2023-09-13 22:39:50 -0700

    feat(greetd): regreet -> tuigreet

--- modules/nixos/windowmanager/greetd.nix
+++ modules/nixos/windowmanager/greetd.nix
@@ -9,24 +9,12 @@
 in
   with lib; {
     config = mkIf cfg.enable {
-      environment.systemPackages = with pkgs; [
-        (catppuccin-gtk.override {
-          accents = ["mauve"];
-          size = "compact";
-          variant = "mocha";
-        })
-        bibata-cursors
-        papirus-icon-theme
-      ];
-
-      programs.regreet = {
+      services.greetd = {
         enable = true;
         settings = {
-          GTK = {
-            cursor_theme_name = "Bibata-Modern-Classic";
-            font_name = "Jost * 12";
-            icon_theme_name = "Papirus-Dark";
-            theme_name = "Catppuccin-Mocha-Compact-Mauve-Dark";
+          default_session = {
+            command = "${pkgs.greetd.tuigreet}/bin/tuigreet --remember-user-session --remember --asterisks --time --cmd Hyprland";
+            user = "greeter";
           };
         };
       };
