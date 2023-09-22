1bce7895704f63d7adb626478000688f2fbc64eb
adace123 <18275490+adace123@users.noreply.github.com>
2023-09-21 23:21:58 -0700

    feat(discord): swap out webcord due to nvidia issues

--- modules/home/desktop/social/discord.nix
+++ modules/home/desktop/social/discord.nix
@@ -5,22 +5,10 @@
   ...
 }: let
   cfg = config.modules.desktop.social.discord;
-  catppuccin-macchiato = pkgs.fetchFromGitHub {
-    owner = "catppuccin";
-    repo = "discord";
-    rev = "0f2c393b11dd8174002803835ef7640635100ca3";
-    hash = "sha256-iUnLLAQVMXFLyoB3wgYqUTx5SafLkvtOXK6C8EHK/nI=";
-  };
 in
   with lib; {
     options.modules.desktop.social.discord.enable = mkEnableOption "discord";
     config = mkIf cfg.enable {
-      home.packages = with pkgs; [webcord-vencord discordo];
-
-      xdg.configFile = {
-        "WebCord/Themes/mocha" = {
-          source = "${catppuccin-macchiato}/themes/mocha.theme.css";
-        };
-      };
+      home.packages = [pkgs.discord];
     };
   }
