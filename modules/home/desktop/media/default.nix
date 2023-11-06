f5fa69ef09b4a44180daa65bb2837aec5619ffc5
adace123 <18275490+adace123@users.noreply.github.com>
2023-11-06 00:10:37 -0800

    remove mpv for now, add cava and playerctl

--- modules/home/desktop/media/default.nix
+++ modules/home/desktop/media/default.nix
@@ -8,8 +8,9 @@
 in
   with lib; {
     options.modules.desktop.media.enable = mkEnableOption "media";
-    imports = [./cava.nix];
+    imports = [./cava.nix ./music-player.nix];
     config = mkIf cfg.enable {
+      services.playerctld.enable = true;
       home.packages = [pkgs.playerctl];
     };
   }
