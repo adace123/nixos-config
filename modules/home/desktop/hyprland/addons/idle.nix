227c4b8fd330bfb2fc562d0c80511f55750b26b4
adace123 <18275490+adace123@users.noreply.github.com>
2023-11-05 23:55:13 -0800

    gtklock -> swaylock

--- modules/home/desktop/hyprland/addons/idle.nix
+++ modules/home/desktop/hyprland/addons/idle.nix
@@ -18,22 +18,42 @@ in
     config = mkIf cfg.enable {
       home.packages = [pkgs.swayidle];
 
+      programs.swaylock = {
+        enable = true;
+        package = pkgs.swaylock-effects;
+        settings = {
+          daemonize = true;
+          clock = true;
+          screenshots = true;
+          indicator = true;
+          indicator-radius = 100;
+          indicator-thickness = 7;
+          effect-blur = "7x5";
+          effect-vignette = "0.5:0.5";
+          ring-color = "3b4252";
+          key-hl-color = "880033";
+          line-color = "00000000";
+          inside-color = "00000088";
+          separator-color = "00000000";
+        };
+      };
+
       services.swayidle = {
         enable = true;
         events = [
           {
-            event = "before-sleep";
-            command = "${pkgs.gtklock}/bin/gtklock -d";
+            event = "lock";
+            command = "${pkgs.swaylock-effects}/bin/swaylock -fF";
           }
           {
-            event = "lock";
-            command = "${pkgs.gtklock}/bin/gtklock -d";
+            event = "before-sleep";
+            command = "${pkgs.systemd}/bin/loginctl lock-session";
           }
         ];
         timeouts = [
           {
             inherit (cfg) timeout;
-            command = "${pkgs.gtklock}/bin/gtklock -d";
+            command = "swaylock";
           }
           {
             timeout = cfg.timeout + 320;
