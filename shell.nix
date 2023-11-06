3c331b2cc412070355c8417f714d6355d03911d4
adace123 <18275490+adace123@users.noreply.github.com>
2023-11-06 00:12:29 -0800

    update shell packages

--- shell.nix
+++ shell.nix
@@ -6,8 +6,6 @@
     inherit (self.checks.${pkgs.system}.pre-commit-check) shellHook;
     nativeBuildInputs = with pkgs; [
       alejandra
-      pulumi
-      pulumiPackages.pulumi-language-nodejs
       sops
       ssh-to-age
     ];
