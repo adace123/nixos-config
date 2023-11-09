32c25fd01cabfada71fd697482f54c412790d84e
adace123 <18275490+adace123@users.noreply.github.com>
2023-11-08 21:18:50 -0800

    add more neovim colorschemes

--- modules/home/desktop/editors/neovim/config/colorscheme.nix
+++ modules/home/desktop/editors/neovim/config/colorscheme.nix
@@ -1,15 +1,24 @@
-{
-  programs.nixvim.colorschemes = {
-    catppuccin = {
-      enable = true;
-      flavour = "mocha";
-      integrations = {
-        cmp = true;
-        treesitter = true;
-        telescope = true;
-        indent_blankline.enabled = true;
-        barbar = true;
+{pkgs, ...}: {
+  programs.nixvim = {
+    colorschemes = {
+      catppuccin = {
+        enable = true;
+        flavour = "mocha";
+        integrations = {
+          cmp = true;
+          treesitter = true;
+          telescope = true;
+          indent_blankline.enabled = true;
+          barbar = true;
+        };
       };
     };
+    extraPlugins = with pkgs.vimPlugins; [
+      tokyonight-nvim
+      gruvbox
+      nord-nvim
+      everforest
+      kanagawa-nvim
+    ];
   };
 }
