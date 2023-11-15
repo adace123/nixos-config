9088ab08fd94ca273010539e24f44f06fe2a74eb
adace123 <18275490+adace123@users.noreply.github.com>
2023-11-14 22:30:39 -0800

    add nixpkgs-latest to flake

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs-latest.url = "github:NixOS/nixpkgs";
     disko = {
       url = "github:nix-community/disko";
       inputs.nixpkgs.follows = "nixpkgs";
