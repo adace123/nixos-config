63e565815fd7489f2c4eb3dedb15f42bc120fcbe
adace123 <18275490+adace123@users.noreply.github.com>
2023-11-14 22:31:27 -0800

    add more fonts

--- hosts/common/font.nix
+++ hosts/common/font.nix
@@ -1,8 +1,17 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  inputs,
+  ...
+}: let
+  nixpkgs = import inputs.nixpkgs-latest {inherit (pkgs) system;};
+in {
   fonts.packages = with pkgs; [
     material-symbols
     roboto
     (nerdfonts.override {fonts = ["FiraCode" "JetBrainsMono"];})
     font-awesome
+    nixpkgs.monaspace
+    fira-code-symbols
+    source-code-pro
   ];
 }
