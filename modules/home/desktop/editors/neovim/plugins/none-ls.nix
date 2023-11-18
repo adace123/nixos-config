c4e0ca4d1d2fcd88bf937958e9d1dad39da82d9c
adace123 <18275490+adace123@users.noreply.github.com>
2023-11-18 00:44:38 -0800

    add lsp-format plugin and eslint

--- modules/home/desktop/editors/neovim/plugins/none-ls.nix
+++ modules/home/desktop/editors/neovim/plugins/none-ls.nix
@@ -1,38 +1,33 @@
 {
-  programs.nixvim.plugins.none-ls = {
-    enable = true;
+  programs.nixvim.plugins = {
+    lsp-format.enable = true;
+    none-ls = {
+      enable = true;
 
-    sources = {
-      formatting = {
-        alejandra.enable = true;
-        black.enable = true;
-        isort.enable = true;
-        gofmt.enable = true;
-        rustfmt.enable = true;
-        stylua.enable = true;
-        shfmt.enable = true;
-        taplo.enable = true;
-        prettier.enable = true;
+      sources = {
+        formatting = {
+          alejandra.enable = true;
+          black.enable = true;
+          isort.enable = true;
+          gofmt.enable = true;
+          rustfmt.enable = true;
+          stylua.enable = true;
+          shfmt.enable = true;
+          taplo.enable = true;
+          prettier.enable = true;
+          eslint_d.enable = true;
+        };
+        diagnostics = {
+          deadnix.enable = true;
+          shellcheck.enable = true;
+          statix.enable = true;
+        };
       };
-      diagnostics = {
-        deadnix.enable = true;
-        shellcheck.enable = true;
-        statix.enable = true;
-      };
-    };
-    onAttach = ''
-      function(client, bufnr)
-        if client.supports_method("textDocument/formatting") then
-          vim.api.nvim_clear_autocmds({ group = vim.api.nvim_create_augroup("LspFormatting", {}), buffer = bufnr })
-          vim.api.nvim_create_autocmd("BufWritePre", {
-            group = vim.api.nvim_create_augroup("LspFormatting", {}),
-            buffer = bufnr,
-            callback = function()
-                vim.lsp.buf.format({ async = false })
-            end,
-          })
+      onAttach = ''
+        function(client, bufnr)
+          require("lsp-format").on_attach(client)
         end
-      end
-    '';
+      '';
+    };
   };
 }
