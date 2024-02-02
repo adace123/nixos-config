d086047f01f3a71d811cb8202a19b0f53ff332b0
aaron.feigenbaum <aaron@enervee.com>
2024-02-02 01:18:26 -0800

    try nvme0n1

--- hosts/coruscant/disk.nix
+++ hosts/coruscant/disk.nix
@@ -6,7 +6,7 @@
   disko.devices.disk = {
     ssd = {
       type = "disk";
-      device = "/dev/sda";
+      device = "/dev/nvme0n1";
       content = {
         type = "gpt";
         partitions = {
