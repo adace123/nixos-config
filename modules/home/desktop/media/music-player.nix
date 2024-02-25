2589b53facab17057e6b23f784227f9e8ee11a4e
adace123 <18275490+adace123@users.noreply.github.com>
2024-02-25 02:00:48 -0800

    overhaul mpv config

--- modules/home/desktop/media/music-player.nix
+++ modules/home/desktop/media/music-player.nix
@@ -8,8 +8,29 @@
 in
   with lib; {
     config = mkIf cfg.enable {
-      home.packages = [pkgs.mpv];
-      services.mpd.enable = true;
-      services.mpd-mpris.enable = true;
+      programs.mpv = {
+        enable = true;
+        scripts = with pkgs.mpvScripts; [
+          mpris
+        ];
+        config = {
+          volume = 60;
+        };
+        bindings = {
+          SPACE = "cycle pause";
+          UP = "add volume 2";
+          DOWN = "add volume -2";
+          h = "seek -5";
+          j = "seek -60";
+          k = "seek 60";
+          l = "seek 5";
+
+          # rebind lost l binding, matches across from L which loops whole file
+          H = "ab-loop";
+          # rebind lost j binding, move J to K
+          J = "cycle sub";
+          K = "cycle sub down";
+        };
+      };
     };
   }
