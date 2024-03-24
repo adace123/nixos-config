6605a252ef1115a7f5a3d2473177e9f819bc2dd5
aaron.feigenbaum <aaron@enervee.com>
2024-03-24 14:15:40 -0700

    fix: make repl config compatible with macos

--- repl.nix
+++ repl.nix
@@ -1,6 +1,4 @@
 let
-  currentHostname = builtins.head (builtins.match "([a-zA-Z0-9]+)\n" (builtins.readFile "/etc/hostname"));
   flake = builtins.getFlake (toString ./.);
 in
-  {hostname ? currentHostname}:
-    flake // flake.nixosConfigurations.${hostname} // {inherit (flake.nixosConfigurations.${hostname}.pkgs) lib;}
+  flake
