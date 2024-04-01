9e4d9d4f26109952ec3fbcda4d5792452343e808
aaron.feigenbaum <aaron@enervee.com>
2024-03-31 18:12:29 -0700

    feat: switch back to neofetch, use theme from neofetch-themes repo

--- modules/home/shell/neofetch/default.nix
+++ modules/home/shell/neofetch/default.nix
@@ -5,18 +5,23 @@
   ...
 }: let
   cfg = config.modules.shell.neofetch;
+  neofetch_themes = pkgs.fetchFromGitHub {
+    owner = "Chick2D";
+    repo = "neofetch-themes";
+    rev = "main";
+    hash = "sha256-btK1TZg49bASOFxQDY5edE0QM+iSsTQmJko4AiyYlpY=";
+  };
 in
   with lib; {
     options.modules.shell.neofetch.enable = mkEnableOption "neofetch";
     config = mkIf cfg.enable {
       home.packages = [
         pkgs.neofetch
-        pkgs.fastfetch
       ];
 
-      xdg.configFile."neofetch/config.conf".source = ./neofetch.conf;
+      xdg.configFile."neofetch/config.conf".source = "${neofetch_themes}/normal/ozozfetch.conf";
       programs.nushell.extraConfig = ''
-        fastfetch
+        neofetch
       '';
     };
   }
