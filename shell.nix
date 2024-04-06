1a5495fd75b46a1ba3dcdcb2dedf5df7efd18453
aaron.feigenbaum <aaron@enervee.com>
2024-04-06 16:15:28 -0700

    feat: bump pre-commit

--- shell.nix
+++ shell.nix
@@ -4,13 +4,15 @@
 }: {
   default = pkgs.mkShell {
     inherit (self.checks.${pkgs.system}.pre-commit-check) shellHook;
-    nativeBuildInputs = with pkgs; [
-      alejandra
-      sops
-      ssh-to-age
-      just
-      gh
-      pulumi-bin
-    ];
+    nativeBuildInputs = with pkgs;
+      [
+        alejandra
+        sops
+        ssh-to-age
+        just
+        gh
+        pulumi-bin
+      ]
+      ++ self.checks.${pkgs.system}.pre-commit-check.enabledPackages;
   };
 }
