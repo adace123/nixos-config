2a4513771df7bb2579ec1854dea3b2903e29dfb2
aaron.feigenbaum <aaron@enervee.com>
2024-04-29 23:02:27 -0700

    fix(btop): update key

--- modules/home/programs/btop.nix
+++ modules/home/programs/btop.nix
@@ -4,7 +4,7 @@
     settings = {
       color_theme = "tomorrow-night";
       theme_background = false;
-      use_vim_keys = true;
+      vim_keys = true;
       temp_scale = "fahrenheit";
     };
   };
