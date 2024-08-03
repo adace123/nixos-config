b5bbcb18f2e058629587e9bd4e8c497b592c13f6
aaron.feigenbaum <aaron@enervee.com>
2024-08-03 15:15:52 -0700

    feat(nvim): add lsp client to lualine

--- modules/home/desktop/editors/neovim/config/plugins/lualine.nix
+++ modules/home/desktop/editors/neovim/config/plugins/lualine.nix
@@ -9,7 +9,29 @@
         lualine_a = ["mode"];
         lualine_b = ["branch" "diagnostics" "diff"];
         lualine_c = ["filename"];
-        lualine_x = ["filetype"];
+        lualine_x = [
+          {
+            icon = "";
+            name.__raw = ''
+              function()
+                  local msg = ""
+                  local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
+                  local clients = vim.lsp.get_active_clients()
+                  if next(clients) == nil then
+                      return msg
+                  end
+                  for _, client in ipairs(clients) do
+                      local filetypes = client.config.filetypes
+                      if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
+                          return client.name
+                      end
+                  end
+                  return msg
+              end
+            '';
+          }
+          "filetype"
+        ];
         lualine_y = ["progress"];
         lualine_z = ["location"];
       };
