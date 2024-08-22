44b33f17798831d51ca6c0ee6fd7bb7f7644a62d
adace123 <18275490+adace123@users.noreply.github.com>
2024-08-22 00:29:40 -0700

    feat(neovim): update telescope keybinds

--- modules/home/desktop/editors/neovim/config/plugins/telescope.nix
+++ modules/home/desktop/editors/neovim/config/plugins/telescope.nix
@@ -8,8 +8,8 @@
             i = {
               "<esc>" = "close";
               "<C-x>" = "close";
-              "<tab>" = "move_selection_next";
-              "<s-tab>" = "move_selection_previous";
+              "<C-j>" = "move_selection_next";
+              "<C-k>" = "move_selection_previous";
               "<C-/>" = "which_key";
               "<C-u>" = false;
               "<C-d>" = false;
@@ -17,6 +17,7 @@
               "<C-y>" = "results_scrolling_up";
               "<C-U>" = "preview_scrolling_up";
               "<C-D>" = "preview_scrolling_down";
+              "<C-q>".__raw = "require('telescope.actions').send_selected_to_qflist + require('telescope.actions').open_qflist";
             };
           };
         };
