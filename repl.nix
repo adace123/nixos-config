9fd8ea3b8e5a82570076fb56a7434fcd80eeefe9
aaron.feigenbaum <aaron@enervee.com>
2024-09-28 16:43:23 -0700

    feat: update repl.nix

--- repl.nix
+++ repl.nix
@@ -1,7 +1,8 @@
 let
   flake = builtins.getFlake (toString ./.);
 in
-  flake
-  // {
-    pkgs = import flake.inputs.nixpkgs {system = builtins.currentSystem;};
-  }
+flake
+// {
+  pkgs = import flake.inputs.nixpkgs { system = builtins.currentSystem; };
+  endor-hm = flake.darwinConfigurations.endor.config.home-manager.users.aaron;
+}
