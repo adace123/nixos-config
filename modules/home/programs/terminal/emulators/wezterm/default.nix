5fb3d87fedc18f9736f9697d730e2703ddfb7c04
aaron.feigenbaum <aaron@enervee.com>
2024-11-06 23:23:52 -0800

    feat: add isDefaultTerminal option for wezterm

--- modules/home/programs/terminal/emulators/wezterm/default.nix
+++ modules/home/programs/terminal/emulators/wezterm/default.nix
@@ -8,8 +8,12 @@ let
 in
 with lib;
 {
-  options.adace.terminal.emulators.wezterm.enable = mkEnableOption "wezterm";
+  options.adace.terminal.emulators.wezterm = {
+    enable = mkEnableOption "wezterm";
+    isDefaultTerminal = mkEnableOption "Set wezterm as default terminal emulator";
+  };
   config = mkIf cfg.enable {
+    home.sessionVariables.TERMINAL = mkIf cfg.isDefaultTerminal "wezterm";
     programs.wezterm = {
       enable = true;
       extraConfig = builtins.readFile ./config.lua;
